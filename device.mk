47e4c69c02014bd697b64ebaf52f78e2464f01f3
Hazama25 <hazamafawkes@gmail.com>
2023-08-09 11:04:23 +0000

    surya: libmegface -> libmegface_xiaomi (2/2)
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- device.mk
+++ device.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
-    libmegface
+    libmegface_xiaomi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
