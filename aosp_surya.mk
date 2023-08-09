6aa05b12b552d04a4d75d7bf434a188b1d959255
Hazama25 <hazamafawkes@gmail.com>
2023-08-09 06:55:35 +0000

    surya: PixelOS-AOSP bringup
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -8,16 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from surya device
+# Inherit from surya device.
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS-AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_surya
+# Device identifier.
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
 PRODUCT_MANUFACTURER := Xiaomi
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
